9eabb473fbb9c2c08349144e055cbae3430f0194
phạm bảo <phambao11062001@gmail.com>
2024-08-28 17:04:19 +0700

    fix selected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,22 @@
 "use client";
+import { useState } from "react";
 import "../../styles/index.scss";
 import HomePage from "./home/page";
 import Project from "./project/page";
+import { useEffect } from "react";
 
 const Home = () => {
-    const selected = localStorage.getItem("selectedProvider");
-    if (selected === "provider") {
-        return <HomePage />;
-    } else {
+    const [selected, setSelected] = useState<string | null>(null);
+
+    useEffect(() => {
+        const storedProvider = localStorage.getItem("selectedProvider");
+        setSelected(storedProvider);
+    }, []);
+
+    if (selected === "cloud") {
         return <Project />;
+    } else {
+        return <HomePage />;
     }
 };
 
